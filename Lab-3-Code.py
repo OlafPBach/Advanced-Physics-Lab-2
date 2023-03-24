5ae7eb112be816bd5175e9b81d03776dd57c8d41
OlafPBach <114641289+OlafPBach@users.noreply.github.com>
2023-03-24 16:55:42 -0400

    Create Lab-3-Code.py

---
+++ Lab-3-Code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "https://raw.githubusercontent.com/OlafPBach/Advanced-Physics-Lab-2/main/Lab-3-Annual_Surface_Temperature_Change.csv"
+data = pd.read_csv(path, quotechar='"', skipinitialspace=True)
+
+NpData = data.to_numpy()
+
+USTempData = (NpData[214,10:])
+
+Years = np.arange(1961, 2022, 1).tolist()
+
+fig, ax = plt.subplots()
+ax.plot(Years,USTempData)
+plt.show()
