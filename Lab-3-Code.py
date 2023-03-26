a5d8727bd5e26fad41f36fa7bcc92e0bb02f8f49
OlafPBach <114641289+OlafPBach@users.noreply.github.com>
2023-03-25 22:15:19 -0400

    Update Lab-3-Code.py

--- Lab-3-Code.py
+++ Lab-3-Code.py
@@ -13,4 +13,7 @@ Years = np.arange(1961, 2022, 1).tolist()
 
 fig, ax = plt.subplots()
 ax.plot(Years,USTempData)
+ax.set_xlabel('Year')
+ax.set_ylabel('Change of Average Temperature')
+ax.set_title('The Effects of Climate Change on Average Temperature')
 plt.show()
